45c5fe8e34c83382e850c60b7a66dc7f6ec8245c
SoumenBaral <shumen.sb@gmail.com>
2023-11-20 01:20:26 +0600

    group added '

---
+++ 2.Deep Drive into DataBase/1.Functions/group.sql
@@ -0,0 +1,41 @@
+USE dummydb;
+
+SELECT * 
+FROM employees;
+
+SELECT MAX(salary)
+FROM employees;
+
+SELECT MIN(salary)
+FROM employees;
+
+SELECT AVG(salary)
+FROM employees;
+
+SELECT COUNT(salary)
+FROM employees;
+
+SELECT COUNT(*)  #COUNT ALL THE ROW
+FROM employees;
+
+SELECT SUM(salary)
+FROM employees;
+
+
+SELECT department_id , MAX(salary)
+FROM employees
+GROUP BY department_id;
+
+
+SELECT department_id , MAX(salary)
+FROM employees
+WHERE department_id != 20
+GROUP BY department_id
+Having(MAX(salary))<5000;
+
+
+SELECT department_id , MAX(salary) as sal
+FROM employees
+WHERE department_id != 20
+GROUP BY department_id
+Having(sal)<5000;
